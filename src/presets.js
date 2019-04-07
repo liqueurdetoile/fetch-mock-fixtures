823f7fd4ccc106cc0e423daafed4a55c2324d79f
Liqueur de Toile <contact@liqueurdetoile.com>
2019-04-07 15:05:50 +0200

    refactor(presets): Add content-type 'text/html' to statuses preset response

--- src/presets.js
+++ src/presets.js
@@ -33,36 +33,42 @@ export default {
 
   204: {
     body: null,
+    headers: {'content-type': 'text/html'},
     status: 204,
     statusText: 'No Content'
   },
 
   400: {
     body: null,
+    headers: {'content-type': 'text/html'},
     status: 400,
     statusText: 'Bad Request'
   },
 
   401: {
     body: null,
+    headers: {'content-type': 'text/html'},
     status: 401,
     statusText: 'Unauthorized'
   },
 
   403: {
     body: null,
+    headers: {'content-type': 'text/html'},
     status: 403,
     statusText: 'Forbidden'
   },
 
   404: {
     body: null,
+    headers: {'content-type': 'text/html'},
     status: 404,
     statusText: 'Not found'
   },
 
   500: {
     body: null,
+    headers: {'content-type': 'text/html'},
     status: 500,
     statusText: 'Internal Server Error'
   }
